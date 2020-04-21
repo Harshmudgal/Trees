ed30b481c14ed997146118f1e80626ec59d6552b
Harshmudgal <54793809+Harshmudgal@users.noreply.github.com>
2020-04-21 13:29:42 +0530

    Update bst_rotation.c

--- bst_rotation.c
+++ bst_rotation.c
@@ -35,18 +35,42 @@ void createNode(struct node* t)
 }
 int imbalance(struct node*q)
 {
-    if(q=="NULL")
+    // if(q=="NULL")
+    // return 0;
+    // if(q->left=="NULL"&&q->right=="NULL")
+    // return 1;
+    int a=height(q->left);
+    int b=height(q->right);
+    if(a-b>1||a-b<-1)
     return 0;
-    if(q->left=="NULL"&&q->right=="NULL")
-    return 1;
-    int a=imbalance(q->left);
-    int b=imbalance(q->right);
-    return a-b;
+    else
+        return 1;
 }
+int height(struct node*t)
+{
+    if(t=="NULL")
+    return 0;
+    if(t->left=="NULL" && t->right=="NULL")
+    return 0;
+    int a=height(t->left);
+    int b=height(t->right);
+    return 1+(a>=b)?a:b;
+} 
 int main()
 {
    printf("Enter root element");
    scanf("%d",head->x);
    createNode(head);
-   int x=imbalance(head);
+   struct node*p;
+   while(p->left=="NULL"&&p->right=="NULL")
+   {
+        int x=imbalance(p);
+        if(x==0)
+        printf("Imbalance node %d",&p->data);
+        else
+        {
+            printf("Balance node %d",&p->data);
+        }
+        p=p->left;
+   }
 }
